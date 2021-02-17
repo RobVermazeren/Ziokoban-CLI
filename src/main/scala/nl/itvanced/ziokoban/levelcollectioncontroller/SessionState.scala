1688c3a425c4e980a7dc5037344e6b4271eb35a9
Rob Vermazeren <rob.vermazeren@itvanced.nl>
2021-02-17 16:07:09 +0100

    Add SessionState.updated

--- src/main/scala/nl/itvanced/ziokoban/levelcollectioncontroller/SessionState.scala
+++ src/main/scala/nl/itvanced/ziokoban/levelcollectioncontroller/SessionState.scala
@@ -3,10 +3,24 @@ package nl.itvanced.ziokoban.levelcollectioncontroller
 import nl.itvanced.ziokoban.model.LevelCollection
 
 final case class SessionState private (
-  currentLevel: Int,
+  currentLevelIndex: Int,
   solvedLevels: Vector[Boolean]
 ) {
   val numberOfLevels = solvedLevels.size
+
+  def updated(levelIndex: Int, solved: Boolean): SessionState = {
+    val newSolvedLevels = solvedLevels.updated(levelIndex, solved)
+
+    def firstAfter(i: Int): Option[Int] = (i until numberOfLevels).find(!solvedLevels(_)) 
+    def firstBefore(i: Int): Option[Int] = (0 until i).find(!solvedLevels(_)) 
+
+    val newCurrentLevelIndex =  
+      firstAfter(levelIndex)
+        .orElse(firstBefore(levelIndex))
+        .getOrElse(0)
+    
+    SessionState(newCurrentLevelIndex, newSolvedLevels)
+  }
 }
 
 object SessionState {
