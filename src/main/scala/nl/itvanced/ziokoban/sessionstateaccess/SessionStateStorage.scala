b738e7eb19bf66876d3aafc33e3305e6c8c9055d
Rob Vermazeren <rob.vermazeren@itvanced.nl>
2021-03-20 17:13:13 +0100

    Use bracket for file io.

--- src/main/scala/nl/itvanced/ziokoban/sessionstateaccess/SessionStateStorage.scala
+++ src/main/scala/nl/itvanced/ziokoban/sessionstateaccess/SessionStateStorage.scala
@@ -1,7 +1,8 @@
 package nl.itvanced.ziokoban.sessionstateaccess
 
-import zio.Task
+import zio.{Task, UIO}
 import nl.itvanced.ziokoban.model.LevelId
+import java.io.FileWriter
 
 object SessionStateStorage {
   import io.circe._
@@ -11,15 +12,33 @@ object SessionStateStorage {
   /*
     Storage in JSON format.
    */
+  def readFromFile(file: os.Path): Task[SessionState] = 
+    createIoSource(file).bracket(closeIoSource){
+      source => 
+        Task.fromEither {
+          val jsonString = source.getLines().mkString
+          parser.decode[SessionState](jsonString)
+        }
+    }
 
-  def readFromFile(file: os.Path): Task[SessionState] = Task.fromEither {
-    val jsonString = os.read(file)
-    parser.decode[SessionState](jsonString)
-  }
+  def writeToFile(file: os.Path, ss: SessionState): Task[Unit] = 
+    createFileWriter(file).bracket(closeFileWriter){
+      writer =>
+        Task.effect { 
+          val jsonString = ss.asJson.spaces2
+          writer.write(jsonString)
+        }
+    }
 
-  def writeToFile(file: os.Path, ss: SessionState): Task[Unit] = Task.effect {
-    val jsonString = ss.asJson.spaces2
-    os.write(file, jsonString)
-  }
+  private def createIoSource(file: os.Path): Task[scala.io.Source] =
+    Task.effect(scala.io.Source.fromInputStream(file.getInputStream))  
   
+  private def closeIoSource(source: scala.io.Source): UIO[Unit] =
+    UIO.effectTotal(source.close()) 
+
+  private def createFileWriter(file: os.Path): Task[FileWriter] = 
+    Task.effect(new FileWriter(file.toIO))  
+
+  private def closeFileWriter(writer: FileWriter): UIO[Unit] = 
+    UIO.effectTotal(writer.close())  
 }
